2d44a07babe6b93dd765bc1252ce5b71cafeea1a
Wembley G. Leach, Jr <wembley.gl@icloud.com>
2023-04-15 16:04:05 -0400

    Implement lighting up LED display in a spiral

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,62 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::timer::Timer,
+    hal::prelude::*,
+};
+
+const DURATION_MS: u32 = 30;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut led_grid = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut row = 0;
+    let mut col = 0;
+
+    loop {
+        // 1. Print the current configuration of LED grid at (row, col)
+        display.show(&mut timer, led_grid, DURATION_MS);
+
+        // 2. Turn off the pin at the current (row, col) –- set the position to 0
+        led_grid[row][col] = 0;
+
+        // 3. Clear the display and delay for 1000ms
+        display.clear();
+        timer.delay_ms(DURATION_MS);
+
+        // 4. Calculate the next pin to light up by modifying `row` and `col` variables
+        match (row, col) {
+            (0, c) if c < 4 => col += 1,
+            (0, c) if c == 4 => row = 1,
+            (r, 4) if r < 4 => row += 1,
+            (4, c) if c > 0 => col -= 1,
+            (r, 0) if r > 0 => row -= 1,
+            (_, _) => {
+                row = 0;
+                col = 0;
+            }
+        }
+
+        // 5. Update the LED grid at (row, col) to be 1
+        led_grid[row][col] = 1;
+
+        // 6. Continue back up to the top of the loop
+    }
 }
